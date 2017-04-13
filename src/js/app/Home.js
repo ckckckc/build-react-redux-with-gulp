c3024dd28281e92301c7f70e70ad4f880123ead6
ckckckc <refala246@gmail.com>
2017-04-13 23:17:52 +0800

    Simplify Home Component

--- src/js/app/Home.js
+++ src/js/app/Home.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Home extends Component {
-  render() {
-    return (
-      <div class="home">
-        <h1 class="home-title">Home</h1>
-      </div>
-    );
-  }
-}
+export default () => {
+  return (
+    <div class="home">
+      <h1 class="home-title">Home</h1>
+    </div>
+  );
+};
